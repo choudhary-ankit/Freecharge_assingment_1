b9c11d87cc54c2507b7532593338fe5713b9a368
choudhary-ankit <ankit_chy@outlook.com>
2020-10-03 14:39:46 +0530

    Updates

---
+++ precache-manifest.78ca0b2d9c93741f305d26d8e2ee67e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc503e64c2f23a11c81f2449cf2dcf41",
+    "url": "/Freecharge_assingment_1/index.html"
+  },
+  {
+    "revision": "002439578441d77689ff",
+    "url": "/Freecharge_assingment_1/static/css/main.01b170ae.chunk.css"
+  },
+  {
+    "revision": "dc9f205e047c84d8d79d",
+    "url": "/Freecharge_assingment_1/static/js/2.012f75ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Freecharge_assingment_1/static/js/2.012f75ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002439578441d77689ff",
+    "url": "/Freecharge_assingment_1/static/js/main.2285175c.chunk.js"
+  },
+  {
+    "revision": "c460636b2d767be8e734",
+    "url": "/Freecharge_assingment_1/static/js/runtime-main.a764fd7f.js"
+  }
+]);
